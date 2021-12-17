fecc027c1115bc5f5adf5fff94a6852bb3545cb2
Fabio2222 <fabiomoreira73@outlook.com>
2021-12-17 18:15:16 -0300

    first commit

---
+++ Lista de compras/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Lista_de_compras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_produtos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lst_compras = new System.Windows.Forms.ListBox();
+            this.label_total = new System.Windows.Forms.Label();
+            this.cmd_nova_compra = new System.Windows.Forms.Button();
+            this.cmd_finalizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lst_produtos
+            // 
+            this.lst_produtos.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst_produtos.FormattingEnabled = true;
+            this.lst_produtos.ItemHeight = 18;
+            this.lst_produtos.Location = new System.Drawing.Point(12, 24);
+            this.lst_produtos.Name = "lst_produtos";
+            this.lst_produtos.Size = new System.Drawing.Size(246, 274);
+            this.lst_produtos.TabIndex = 0;
+            this.lst_produtos.DoubleClick += new System.EventHandler(this.lst_produtos_DoubleClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PRODUTOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "COMPRAS";
+            // 
+            // lst_compras
+            // 
+            this.lst_compras.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst_compras.FormattingEnabled = true;
+            this.lst_compras.ItemHeight = 18;
+            this.lst_compras.Location = new System.Drawing.Point(264, 24);
+            this.lst_compras.Name = "lst_compras";
+            this.lst_compras.Size = new System.Drawing.Size(244, 220);
+            this.lst_compras.TabIndex = 2;
+            // 
+            // label_total
+            // 
+            this.label_total.BackColor = System.Drawing.Color.White;
+            this.label_total.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_total.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_total.Location = new System.Drawing.Point(261, 247);
+            this.label_total.Name = "label_total";
+            this.label_total.Size = new System.Drawing.Size(247, 51);
+            this.label_total.TabIndex = 4;
+            this.label_total.Text = "COMPRAS";
+            this.label_total.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cmd_nova_compra
+            // 
+            this.cmd_nova_compra.Location = new System.Drawing.Point(12, 304);
+            this.cmd_nova_compra.Name = "cmd_nova_compra";
+            this.cmd_nova_compra.Size = new System.Drawing.Size(126, 45);
+            this.cmd_nova_compra.TabIndex = 5;
+            this.cmd_nova_compra.Text = "NOVA COMPRA";
+            this.cmd_nova_compra.UseVisualStyleBackColor = true;
+            this.cmd_nova_compra.Click += new System.EventHandler(this.cmd_nova_compra_Click_1);
+            // 
+            // cmd_finalizar
+            // 
+            this.cmd_finalizar.Location = new System.Drawing.Point(382, 304);
+            this.cmd_finalizar.Name = "cmd_finalizar";
+            this.cmd_finalizar.Size = new System.Drawing.Size(126, 45);
+            this.cmd_finalizar.TabIndex = 6;
+            this.cmd_finalizar.Text = "FINALIZAR";
+            this.cmd_finalizar.UseVisualStyleBackColor = true;
+            this.cmd_finalizar.Click += new System.EventHandler(this.cmd_finalizar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 361);
+            this.Controls.Add(this.cmd_finalizar);
+            this.Controls.Add(this.cmd_nova_compra);
+            this.Controls.Add(this.label_total);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lst_compras);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lst_produtos);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_produtos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lst_compras;
+        private System.Windows.Forms.Label label_total;
+        private System.Windows.Forms.Button cmd_nova_compra;
+        private System.Windows.Forms.Button cmd_finalizar;
+    }
+}
+
---
+++ Lista de compras/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_de_compras
+{
+    public partial class Form1 : Form
+    {
+        class produto
+        {
+            public string nome;
+            public double preco;
+        }
+
+        List<produto> LISTA_PRODUTOS;
+        List<produto> LISTA_COMPRAS;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            LISTA_PRODUTOS = new List<produto>()
+            {
+                new produto(){ nome = "Laranja", preco = 10 },
+                new produto(){ nome = "Ananás", preco = 25 },
+                new produto(){ nome = "Banana", preco = 5.5 }
+            };
+
+            foreach (produto p in LISTA_PRODUTOS)
+            {
+                lst_produtos.Items.Add(ConstruirLinhaProduto(p));
+            }
+
+            IniciarCompras();
+        }
+
+        private string ConstruirLinhaProduto(produto p)
+        {
+            string preco = p.preco.ToString("0.00") + " $";
+            return p.nome + new string(' ', 30 - p.nome.Length - preco.Length) + preco;
+        }
+
+        private void IniciarCompras()
+        {
+            LISTA_COMPRAS = new List<produto>();
+            lst_compras.Items.Clear();
+            label_total.Text = "0,00 $";
+        }
+
+        private void lst_produtos_DoubleClick_1(object sender, EventArgs e)
+        {
+            if (lst_produtos.SelectedIndex == -1) return;
+            produto p = LISTA_PRODUTOS[lst_produtos.SelectedIndex];
+            AdicionarProdutoCompra(p);
+        }
+
+        private void AdicionarProdutoCompra(produto p)
+        {
+            LISTA_COMPRAS.Add(p);
+            lst_compras.Items.Add(ConstruirLinhaProduto(p));
+
+            var total = LISTA_COMPRAS.Sum(i => i.preco);
+            label_total.Text = total.ToString("0.00") + " $";
+        }
+
+        private void cmd_nova_compra_Click_1(object sender, EventArgs e)
+        {
+            IniciarCompras();
+        }
+
+        private void cmd_finalizar_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Conta finalizada!" + Environment.NewLine + label_total.Text);
+
+        }                         
+    }    
+}
